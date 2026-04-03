2d1b15cdcbfdd3738dd059b22c8834ade89eacdc
Nhieu Minh Truong <mnhieu04@gmail.com>
2026-04-04 00:07:52 +0700

    feat add login

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import ProductDetailPage from './pages/ProductDetailPage'
 import OrdersPage from './components/OrdersPage'
 import ShoppingCartPage from './pages/ShoppingCartPage'
 import EmptyCartPage from './pages/EmptyCartPage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/orders" element={<OrdersPage />} />
         <Route path="/cart" element={<ShoppingCartPage />} />
         <Route path="/cart/empty" element={<EmptyCartPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   )
